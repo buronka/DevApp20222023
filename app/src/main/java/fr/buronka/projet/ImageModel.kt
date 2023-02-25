adb282d7c534e5c3f0e95e2a47467776b2498c06
Thibault Pradal <thibault.pradal@yahoo.fr>
2023-02-25 15:03:19 +0100

    Added ID var imagemodel class

--- app/src/main/java/fr/buronka/projet/ImageModel.kt
+++ app/src/main/java/fr/buronka/projet/ImageModel.kt
@@ -4,7 +4,8 @@ class ImageModel(
     val date : String = "Date prise photo",
     val heure : String = "Heure prise photo",
     val imageUrl: String = "http://buronka.com/imagefaceface.jpg",
-    val resolved : Boolean = false
+    val resolved : Boolean = false,
+    val id : String = "image id"
 )
 
 //class ImageModel(
