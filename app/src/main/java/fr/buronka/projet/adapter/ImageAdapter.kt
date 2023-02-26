2b3ae15d26c5813624aeca03d4ab9145efc4b50a
Thibault Pradal <thibault.pradal@yahoo.fr>
2023-02-26 12:55:33 +0100

    Error fix

--- app/src/main/java/fr/buronka/projet/adapter/ImageAdapter.kt
+++ app/src/main/java/fr/buronka/projet/adapter/ImageAdapter.kt
@@ -76,7 +76,7 @@ class ImageAdapter(val context: MainActivity,
     }
 
     //renvoie le nombre d'items afficher dynamiquement
-    override fun getItemCount(): Int = 3
+    override fun getItemCount(): Int = imageList.size
 
 
 }
