2822a563c13b5748a472080fa81411fd37d844ea
Thibault Pradal <thibault.pradal@yahoo.fr>
2023-02-26 16:34:49 +0100

    Add full functionnality to Add img

--- app/src/main/java/fr/buronka/projet/ImageRepository.kt
+++ app/src/main/java/fr/buronka/projet/ImageRepository.kt
@@ -1,14 +1,23 @@
 package fr.buronka.projet
 
+import android.net.Uri
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import fr.buronka.projet.ImageRepository.Singleton.databaseRef
+import fr.buronka.projet.ImageRepository.Singleton.downloadUri
 import fr.buronka.projet.ImageRepository.Singleton.imageList
+import fr.buronka.projet.ImageRepository.Singleton.storageReference
+import java.util.*
 
 class ImageRepository {
     object Singleton{
+        val storageReference = FirebaseStorage.getInstance().getReference("gs:projetdevapp20222023.appspot.com")
 
         //se connecter a la reference "images"
         val databaseRef = FirebaseDatabase.getInstance().getReference("images")
@@ -16,6 +25,8 @@ class ImageRepository {
         //creer une liste qui va contenir nos images
         val imageList = arrayListOf<ImageModel>()
 
+        // Lien image actuelle
+        var downloadUri: Uri? = null
     }
 
     fun updateData(callback: () -> Unit) {
@@ -48,9 +59,38 @@ class ImageRepository {
 
     }
 
-    //mettre a jour dans bdd
+    // Envoi fichier au Storage
+    fun uploadImage(file : Uri, callback: () -> Unit) {
+        // Not Null
+        if(file != null) {
+            val fileName = UUID.randomUUID().toString() + ".jpg"
+            val ref = storageReference.child(fileName)
+            val uploadTask = ref.putFile(file)
+
+            uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> {task ->
+                if(!task.isSuccessful) {
+                    task.exception?.let {throw it}
+                }
+                return@Continuation ref.downloadUrl
+
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    downloadUri = task.result
+                    callback()
+                }
+            }
+        }
+
+    }
+
+    //mettre a jour dans la bdd
     fun updateImage(Image: ImageModel) {
         databaseRef.child(Image.id).setValue(Image)
     }
+
+    fun insertImage(Image: ImageModel) {
+        databaseRef.child(Image.id).setValue(Image)
+    }
+
 
 }
--- app/src/main/java/fr/buronka/projet/fragments/AddImageFrgment.kt
+++ app/src/main/java/fr/buronka/projet/fragments/AddImageFrgment.kt
@@ -2,46 +2,81 @@ package fr.buronka.projet.fragments
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import fr.buronka.projet.ImageModel
+import fr.buronka.projet.ImageRepository
+import fr.buronka.projet.ImageRepository.Singleton.downloadUri
 import fr.buronka.projet.MainActivity
 import fr.buronka.projet.R
+import java.util.*
 
 class AddImageFrgment (
     private val context:MainActivity
 ) : Fragment() {
 
-    //private var uploadedImage:ImageView? = null
+    private var fileS:Uri? = null
+    private var uploadedImage:ImageView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.fragment_add_image, container, false)
 
-        //uploadedImage = view.findViewById(R.id.preview_img)
+        // Recuperer l'image
+        uploadedImage = view.findViewById(R.id.preview_img)
 
-        // Chargement image
+        // Chargement image en récupérant les données
         val pickupImageButton = view.findViewById<Button>(R.id.upload_button)
-        //pickupImageButton.setOnClickListener { pickupImage() }
 
+        pickupImageButton.setOnClickListener { pickupImage() }
+
+        val confirmButton = view.findViewById<Button>(R.id.confirm_button)
+        confirmButton.setOnClickListener { sendForm(view) }
         return view
     }
-/*
-    private fun pickupImage() {
+
+    private fun sendForm(view: View) {
+        val repo = ImageRepository()
+        repo.uploadImage(fileS!!) {
+            val imgDate = view.findViewById<EditText>(R.id.date_input).text.toString()
+            val imgHour = view.findViewById<EditText>(R.id.hour_input).text.toString()
+            val downloadImageUrl = downloadUri
+
+            // Creer un nouvel objet
+            val img = ImageModel(
+                date = imgDate,
+                heure = imgHour,
+                imageUrl = downloadImageUrl.toString(),
+                resolved = false,
+                id = UUID.randomUUID().toString()
+            )
+
+            // Envoi a la bdd
+            repo.insertImage(img)
+        }
+    }
+
+    fun pickupImage() {
         val intent = Intent()
         intent.type = "image/"
         intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 69)
+        resultLauncher.launch(Intent.createChooser(intent, "Select Picture"))
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
-        if(requestCode == 69 && resultCode == Activity.RESULT_OK) {
-            if(data == null || data.data == null) return
-            val selectedImage = data.data
+    var resultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+        if(result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val fileS = data?.data
+
+            // Maj de l'image sur la page
+            uploadedImage?.setImageURI(fileS)
         }
     }
- */
 }
